9a6f837909632114767ad25387f2899a4dda4dcd
bathuleg <batkhuleg0319@gmail.com>
2025-05-14 02:04:55 +0800

    Update StringProcessor.java

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -6,11 +6,19 @@ public class StringProcessor {
 			Predicate<String> filter,
 			Function<String, String> transformer,
 			Consumer<String> printer) {
-		// Хэрэгжүүл: filter, transform, print хий
+		for(String str : strings){
+			if(filter.test(str)){
+				String transformed=transformer.aplly(str);
+				printer.accept(transformed);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
-		// Хэрэгжүүл: Predicate, Function, Consumer-ийг тодорхойлж, processStrings дууд
+		Predicate <String> longerThan5=s->s.length() > 5;
+		Function <String, String> reverse=s-> new StringBuilder(s).reverse().toString();
+		Consumer <String> printHeadline=s-> System.out.println("GARSHIL: "+s.toUpperCase());
+		processString(words,longerThan5, reverse, printHeadline);
 	}
 }
